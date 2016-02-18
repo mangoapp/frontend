a4d3ce3e42a21efbc2175da61e96ffffcf8954df
Shane DeWael <dewaels@purdue.edu>
2016-02-18 14:09:08 -0500

    initiate

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
