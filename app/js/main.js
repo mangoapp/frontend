50df70c8c2fc53568b2a34a646f98f4381783f40
Shane DeWael <dewaels@purdue.edu>
2016-03-03 22:07:21 -0500

    navbar

--- app/js/main.js
+++ app/js/main.js
@@ -45,10 +45,6 @@
             templateUrl: "./views/courses/course.html",
             controller: "CourseCtrl"
         })
-        .when("/settings", {
-          templateUrl: "./views/users/user-settings.html",
-          controller: "UserCtrl"
-        })
         .when("/my-profile", {
           templateUrl: "./views/users/my-profile.html",
           controller: "UserCtrl"
