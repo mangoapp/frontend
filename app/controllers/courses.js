ede0c4f997acae92d06287c0308ff74cfb0f12eb
Shane DeWael <dewaels@purdue.edu>
2016-03-04 10:17:58 -0500

    announcement bug

--- app/controllers/courses.js
+++ app/controllers/courses.js
@@ -89,6 +89,7 @@ module.exports = function($scope,$http,API,auth,$window,$routeParams) {
 		};
 		$http(req).then(function(res) {
 			console.log("Announcement added");
+			$window.location.reload();
 		},$scope.handleRequest);
 	};
 	$scope.createSection = function() {
