a12260bb83410494537d60527d6bd460f6eedc15
Shane DeWael <dewaels@purdue.edu>
2016-04-06 14:11:00 -0400

    get threads

--- app/controllers/discussion.js
+++ app/controllers/discussion.js
@@ -21,34 +21,53 @@ module.exports = function($scope,$http,API,auth,$window,$routeParams) {
 	};
 
 	$scope.getCourseWithID = function(id) {
-		console.log(id);
 		setTimeout(function(){ 
 			if ($scope.courses) {
-			for (var i = 0; i < $scope.courses.length; i++) {
-				console.log($scope.courses[i]);
-				if ($scope.courses[i].id == id) {
-					$scope.noCourses = false;
-					$scope.courseID = $scope.courses[i].id;
-					$scope.courseName = $scope.courses[i].name;
-					$scope.userRole = $scope.courses[i].role_name;
-					$scope.userDisplayRole = $scope.courses[i].role_display_name;
-					$scope.courseData = true;
-					if ($scope.userRole == 'course_admin') {
-						$scope.isInstructor = true;
+				for (var i = 0; i < $scope.courses.length; i++) {
+					if ($scope.courses[i].id == id) {
+						$scope.noCourses = false;
+						$scope.courseID = $scope.courses[i].id;
+						$scope.courseName = $scope.courses[i].name;
+						$scope.userRole = $scope.courses[i].role_name;
+						$scope.userDisplayRole = $scope.courses[i].role_display_name;
+						$scope.courseData = true;
+						if ($scope.userRole == 'course_admin') {
+							$scope.isInstructor = true;
+						}
+						$scope.$apply();
+						$scope.getCourseThreads();
 					}
-					$scope.$apply();
 				}
 			}
-		}
 		}, 400);
-		
+	};
+
+	$scope.getCourseThreads = function() {
+		$scope.threads=[];
+		$scope.pinnedThreads=[];
+		if ($scope.courseID) {
+			var req = {
+				method: 'GET',
+				headers: {
+					'Authorization': 'Bearer: ' + $scope.token
+				},
+				url: API + '/forum/' + $scope.courseID + '/threads'
+			};
+			$http(req).then(function(res) {
+				if (res.data.threads) {
+					$scope.threads = res.data.threads;
+				}
+				console.log($scope.threads);		
+			},$scope.handleRequest);
+		}
 	};
 
 	$scope.$on('$viewContentLoaded', function() {
 		$scope.getCourses();
-    	if ($routeParams.courseNumber) {
+		if ($routeParams.courseNumber) {
 			$scope.getCourseWithID($routeParams.courseNumber);
 		}
+		
 	});
 
 };
