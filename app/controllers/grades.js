a52e154dca015292c9a316d8f985ee31dfde655a
bmatt <mess@purdue.edu>
2016-04-06 14:08:50 -0400

    added logic to grades controller, component-ified course detail sidebar

--- app/controllers/grades.js
+++ app/controllers/grades.js
@@ -7,48 +7,48 @@ module.exports = function($scope,$http,API,auth,$window,$routeParams) {
 		$scope.loggedin = false;
 	}
 
-	// $scope.getCourses = function() {
-	// 	var req = {
-	// 		method: 'GET',
-	// 		headers: {
-	// 			'Authorization': 'Bearer: ' + $scope.token
-	// 		},
-	// 		url: API + '/users/sections'
-	// 	};
-	// 	$http(req).then(function(res) {
-	// 		$scope.courses = res.data;
-	// 	},$scope.handleRequest);
-	// };
+	$scope.getCourses = function() {
+		var req = {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer: ' + $scope.token
+			},
+			url: API + '/users/sections'
+		};
+		$http(req).then(function(res) {
+			$scope.courses = res.data;
+		},$scope.handleRequest);
+	};
 
-	// $scope.getCourseWithID = function(id) {
-	// 	console.log(id);
-	// 	setTimeout(function(){ 
-	// 		if ($scope.courses) {
-	// 		for (var i = 0; i < $scope.courses.length; i++) {
-	// 			console.log($scope.courses[i]);
-	// 			if ($scope.courses[i].id == id) {
-	// 				$scope.noCourses = false;
-	// 				$scope.courseID = $scope.courses[i].id;
-	// 				$scope.courseName = $scope.courses[i].name;
-	// 				$scope.userRole = $scope.courses[i].role_name;
-	// 				$scope.userDisplayRole = $scope.courses[i].role_display_name;
-	// 				$scope.courseData = true;
-	// 				if ($scope.userRole == 'course_admin') {
-	// 					$scope.isInstructor = true;
-	// 				}
-	// 				$scope.$apply();
-	// 			}
-	// 		}
-	// 	}
-	// 	}, 400);
+	$scope.getCourseWithID = function(id) {
+		console.log(id);
+		setTimeout(function(){ 
+			if ($scope.courses) {
+			for (var i = 0; i < $scope.courses.length; i++) {
+				console.log($scope.courses[i]);
+				if ($scope.courses[i].id == id) {
+					$scope.noCourses = false;
+					$scope.courseID = $scope.courses[i].id;
+					$scope.courseName = $scope.courses[i].name;
+					$scope.userRole = $scope.courses[i].role_name;
+					$scope.userDisplayRole = $scope.courses[i].role_display_name;
+					$scope.courseData = true;
+					if ($scope.userRole == 'course_admin') {
+						$scope.isInstructor = true;
+					}
+					$scope.$apply();
+				}
+			}
+		}
+		}, 400);
 		
-	// };
+	};
 
 	$scope.$on('$viewContentLoaded', function() {
-		// $scope.getCourses();
-  //   	if ($routeParams.courseNumber) {
-		// 	$scope.getCourseWithID($routeParams.courseNumber);
-		// }
+		$scope.getCourses();
+    	if ($routeParams.courseNumber) {
+			$scope.getCourseWithID($routeParams.courseNumber);
+		}
 	});
 
 };
